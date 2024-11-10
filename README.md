2d322d2d108ee22df75fa07c651df8c930ed2b9e
Vlad Filip <vf278@cam.ac.uk>
2024-11-10 13:18:58 +0000

    [224450] uses Redis for storing data

--- README.md
+++ README.md
@@ -1,83 +1,43 @@
-# To-Do List Application
+# To Do List Application 
+This is a JavaScript project to create a to-do list.
 
-This is a JavaScript project for a to-do list application. Yay!
+## Description and user interface
+The application is run at localhost:4444/to-do-list.
 
-## Description
+The user can add a task under one category from the select dropdown. 
+The task can then be marked (i.e., ticked) as completed and move from the oustanding list to the completed tasks.
+This can then be removed by rmeoved by pressing a remove button. 
 
-The application runs at `localhost:4444/to-do-list`.
+There is a category page where new categories can be added with selected colors from the color select element.
+Categories can be removed or reinitialiased to the three default types: home, garden, work. 
+If a category is removed, all the tasks under that category from the task page will also be removed.
+The navigation bar allows transition between the category and tasks pages. 
 
-### Features
+## Installation and running
+To run the app download and open the master folder. 
+You need to have the nodemon and express packages installed. 
+For installation run following command in terminal:
 
-- **Task Management**: Users can add tasks under specific categories from a dropdown menu. Tasks can be marked as completed, moving them from the outstanding list to the completed tasks list. Completed tasks can be removed by pressing a remove button.
-- **Category Management**: Users can add new categories with selected colors from a color picker. Categories can be removed or reset to the default types: Home, Garden, and Work. Removing a category will also remove all associated tasks.
-- **Navigation**: A navigation bar allows users to switch between the category and tasks pages.
+```
+npm init -y
+npm install -g nodemon
+npm install express
+```
+The data between restarts is stored in a Redis or PosgreSQL database depending on which version/branch you run.
+To install either of these modules run:
 
-## Installation and Running
+```
+npm install redis
+npm install pg
+```
 
-### Prerequisites
+After installation, run the dev command and go to local:444 on your browser.
+For this you need to use node version 20.
 
-- Node.js (version 20)
-- npm (Node Package Manager)
-- nvm (Node Version Manager)
-
-### Setup
-
-1. **Clone the Repository**
-
-   ```sh
-   git clone https://github.com/yourusername/yourproject.git
-   cd yourproject
-   ```
-2. **Install Dependencies**
-
-    Ensure you have nodemon and express installed:
-
-    ```sh
-    npm init -y
-    npm install -g nodemon
-    npm install express
-    ```
-
-3. **Database Setup**
-
-    The application supports both Redis and PostgreSQL for data persistence. Depending on the branch you are using:
-    - For Redis, use the feature-22450 branch.
-    - For PostgreSQL, use the master or main branch.
-
-    Install the necessary database modules:
-
-    ```sh
-    npm install redis
-    npm install pg
-    ```
-
-    When using PostgreSQL, you need to initialize the database by runing the bootstrap SQL file:
-
-    ```sh
-    psql -d todoapp -f bootstrap.sql
-    psql -d todoapp <your_username> -f bootstrap.sql # If username required
-    ```
-
-4. **Run the Application**
-
-    Use Node Version Manager (nvm) to switch to Node.js version 20 and start the application:
-
-    ```ah
-    nvm use 20
-    npm run dev
-    ```
-
-    Open your browser and navigate to `http://localhost:4444`.
-
-## Applications and Extensions
-
-### Chrome Extension
-
-A Chrome extension has bee developed to work with this to do list application. The extension displays the task in different tabs while the application is running. It allows the user to update the task without the need to be on the main application tab. The extension can be accessed at: https://github.com/vladlumfilip/Extension-To-Do. 
-
-### Content Manager
-
-This is an application that allows management to make edits to the README file of this project without interacting with the actual code base of the to do list app. The content manager is built using React (https://react.dev/) and can be accessed at: https://github.com/vladlumfilip/Content-Manager.
-
-## Credentials and Acknowledgements
-This application was created by Vlad Filip under the supervision of Ilinca Ion.
+```
+nvm -v # To view current version
+nvm current
+nvm list # To list all versions
+nvm use 20 # To switch
+nmp run dev # To run the application in development mode
+```
--- server.js
+++ server.js
@@ -1,95 +1,138 @@
 // Import dependencies
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require('cors');
-const { Pool } = require('pg');
+const express = require("express")
+const bodyParser = require("body-parser")
+const redis = require("redis");
+const { promisify } = require("util");
 
 // Create a new express application object
 const app = express()
 app.set('view engine', 'ejs');
 
-// Define global variables
+// Define global variables for the two task lists
 var todos = [];
 var ticks = [];
 var categories = [];
-var categoryMap = {};
 
-// PostgreSQL connection pool
-const pool = new Pool({
-    user: 'vlad.filip',
-    host: 'localhost',
-    database: 'todoapp',
-    password: '',
-    port: 5432,
+// Create a Redis client
+// Create a Redis client with correct port and host
+const REDIS_PORT = process.env.REDIS_PORT || 6379; // Default Redis port is 6379
+const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1'; // Default Redis host is localhost
+const client = redis.createClient({
+    host: REDIS_HOST,
+    port: REDIS_PORT
+});
+
+client.on("error", function (error) {
+    console.error(`Redis client not connected to the server: ${error}`);
+});
+
+client.on("connect", function () {
+    console.log("Redis client connected to the server");
 });
 
 // Middleware
-app.use("/static", express.static("static"));
-app.use(express.urlencoded({extended: true}));
+app.use("/static", express.static("static"))
+app.use(express.urlencoded({extended: true}))
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-app.use(express.json());
 
-// Immediately-Invoked Function Expression (IIFE) to ensure PosgreSQL connection at startup
+// Immediately-Invoked Function Expression (IIFE) to ensure Redis client connection at startup
 (async () => {
     try {
+        await client.connect();
+
         // Ensure the store is initialized at the start and handle errors
-        const { todos, ticks, categories, categoryMap } = await initializeStore();
+        const { todos, ticks, categories } = await initializeStore();
     } catch (error) {
-        console.error("Error during PostgreSQL client connection or store initialization:", error);
+        console.error("Error during Redis client connection or store initialization:", error);
         process.exit(1); // Exit the process with failure code
     }
 })();
 
-// Initialize data from PostgreSQL
+// Promisify Redis client methods for easier async/await usage
+const setAsync = promisify(client.set).bind(client);
+
+// Initialize Redis store if not already initialized
 async function initializeStore() {
     try {
         console.log('INITIALIZING STORE');
 
-        // Fetch categories from PostgreSQL
-        console.log("Fetching categories from PosgreSQL...");
-        // Get categories
-        const categoriesResult = await pool.query('SELECT * FROM categories');
-        categories = categoriesResult.rows;
-        // Get tasks
-        const tasksResult = await pool.query('SELECT * FROM tasks');
-        tasks = tasksResult.rows;
+        // Check if categories list exist
+        console.log("Fetching categories from Redis...");
+        if (await client.exists('categories')) {
+            console.log('Categories already initialized.');
+            categories = JSON.parse(await client.get('categories')) || [];;
+        } else {
+            console.log('Categories not found, initializing...');
+            categories = [];
+            categories.push({category: 'Work', color: '#0f74cc'});
+            categories.push({category: 'Home', color: '#5db7c6'});
+            categories.push({category: 'Garden', color: '#6b65da'});
+            setAsync('categories', JSON.stringify(categories));
+            console.log('Initialized.');
+        }
 
-        // Create a mapping from category_id to category_name
-        categoryMap = {};
-        categories.forEach(category => {
-            categoryMap[category.id] = category.name;
-        });
-
-        // Initialize todos and ticks arrays
-        todos = [];
-        ticks = [];
-        for (let task of tasks) {
-            const categoryName = categoryMap[task.category_id];  // Lookup category name
-            if (!task.completed) {
-                // Add elements to outstanding list
-                todos.push({task: task.name, category: categoryName})
+        for (let index in categories) {
+            category = categories[index].category;
+            if (await client.exists(category)) {
+                console.log('Category is already initialized:',category);
+                this[category] = JSON.parse(await client.get(category)) || [];
             } else {
-                // Add elements to completed tasks list
-                ticks.push({ task: task.name, category: categoryName})
+                console.log('Category not found, initializing...');
+                setAsync(category, JSON.stringify([]))
+                console.log('Initialized category:',category);
             }
         }
-        return {todos, ticks, categories, categoryMap};
-        } catch (err) {
-            console.error('Error initializing store:', err);
-            return { todos: [], ticks: [], categories: [], categoryMap: [] };
+
+        // Create todos and ticks from the categories databases
+        todos = [];
+        ticks = [];
+        for (let index in categories) {
+            category = categories[index].category;
+            tasks = JSON.parse(await client.get(category));
+            for (let indexTask in tasks) {
+                if (tasks[indexTask].ticked == 0) {
+                    todos.push({task: tasks[indexTask].task, category: category});
+                } else {
+                    ticks.push({task: tasks[indexTask].task, category: category});
+                }
+            }
+        }
+
+        return {todos, ticks, categories}
+    } catch (err) {
+        console.error('Error initializing store:', err);
+    }
+}
+
+// Write/remove task to Redis
+async function writeTasks(taskNAME, taskVALUE, category) {
+    try {
+        index = this[category].findIndex(t => t.task == taskNAME);
+        if (index > -1) {
+            if (this[category][index].ticked == 0) {
+                this[category][index].ticked = 1;
+                await client.set(category, JSON.stringify(this[category]));
+            } else {
+                this[category].splice(index, 1);
+                await client.set(category, JSON.stringify(this[category]));
+            }
+        } else {
+            this[category].push({task: taskNAME, ticked: taskVALUE});
+            await client.set(category, JSON.stringify(this[category]));
+        }
+    } catch (err) {
+        console.error('Error writing tasks to Redis:', err);
     }
 }
 
 // ROUTES
 app.get("/to-do-list", async (req, res) => {
     try {
-        res.render("./pug/index.pug", {
+        res.render("index.ejs", {
             name: "Vlad Filip",
             todos: todos,
             ticks: ticks,
-            categories: categories,
-            categoryMap: categoryMap
+            categories: categories
         });
     } catch (err) {
         console.error("Error initializing or reading tasks:", err);
@@ -99,12 +142,11 @@ app.get("/to-do-list", async (req, res) => {
 
 app.get("/categories", async (req,res) => {
     try {
-        res.render("./pug/categories.pug", {
+        res.render("categories.ejs", {
             name: "Vlad Filip",
             todos: todos,
             ticks: ticks,
-            categories: categories,
-            categoryMap: categoryMap
+            categories: categories
         });
     } catch (err) {
         console.error("Error fetching categories:", err);
@@ -112,26 +154,6 @@ app.get("/categories", async (req,res) => {
     }
 })
 
-// APIs
-app.get("/api/outstanding-tasks", async (req, res) => {
-    try {
-        res.json(todos);
-    } catch (err) {
-        console.error("Error fetching outstanding tasks:", err);
-        res.status(500).send("Internal Server Error");
-    }
-});
-
-app.get("/api/completed-tasks", async (req, res) => {
-    try {
-        res.json(ticks);
-    } catch (err) {
-        console.error("Error fetching completed tasks:", err);
-        res.status(500).send("Internal Server Error");
-    }
-});
-
-// POST METHODS
 // Read and add task
 app.post("/addtask", async (req, res) => {
     try{ 
@@ -139,12 +161,6 @@ app.post("/addtask", async (req, res) => {
         const newTask = req.body.task;
         // Read category
         const category = req.body.category;
-        // Reverse category map
-        const reverseCategoryMap = Object.fromEntries(
-            Object.entries(categoryMap).map(([id, name]) => [name, id])
-          );
-        // Get category id
-        const categoryId = reverseCategoryMap[category];
         if (newTask && category) {
             // Check the task has not already been added
             index_todos = todos.findIndex(t => t.task === newTask && t.category == category);
@@ -155,7 +171,7 @@ app.post("/addtask", async (req, res) => {
                 // Push new todo into array
                 todos.push({ task: newTask , category: category });
                 // Write tasks in database
-                await pool.query(`INSERT INTO tasks (name, category_id, completed) VALUES ($1, $2, $3)`, [newTask, categoryId ,false])
+                await writeTasks(newTask, 0, category);
                 console.log('Added new task:', newTask);
 
             }
@@ -171,20 +187,16 @@ app.post("/addtask", async (req, res) => {
 app.post("/ticktask", async (req, res) => {
     try {
         // Read ticked task
-        const tickedTask = req.body.check;
-        console.log(tickedTask);
+        const tickedTask = req.body.check;;
         if (tickedTask) {
             // Split the task name and category
             const [taskName, category] = tickedTask.split('_')
             // Remove the task from
             const taskIndex = todos.findIndex(t => t.task === taskName && t.category == category);
             if (taskIndex > -1) {
-                // Delete in todos
-                todos.splice(taskIndex, 1);
-                // Push to ticks
-                ticks.push({ task: taskName, category: category }); 
-                // Update database
-                await pool.query(`UPDATE tasks SET completed = $1 WHERE name = $2`, [true, taskName]);
+                todos.splice(taskIndex, 1); // Delete in todos
+                ticks.push({ task: taskName, category: category }); // Push to ticks
+                await writeTasks(taskName, 1, category) // Update redis
             }
             console.log('Ticked task:', taskName);
             res.redirect('/to-do-list');
@@ -204,15 +216,12 @@ app.post("/removetask", async (req,res) => {
             // Split the task name and category
             const [taskName, category] = tasktoRemove.split('_')
             const taskIndex  = ticks.findIndex(t => t.task === taskName && t.category == category);
-            console.log(taskIndex);
             if (taskIndex > -1) {
                 ticks.splice(taskIndex, 1); // Remove from ticks
-                await pool.query(`DELETE FROM tasks WHERE name = $1`, [taskName]); // Update PostgreSQL
+                await writeTasks(taskName, 1, category); // Update redis database
             }
-            console.log('Removed task:', taskName);
-            res.redirect('/to-do-list');
-        } else {
-            res.status(422).send(`You haven't given me a task`);
+        console.log('Removed task:', taskName);
+        res.redirect('/to-do-list');
         }
     } catch (err) {
         console.error('Error removing task:', err);
@@ -228,24 +237,15 @@ app.post("/addcategory", async (req,res) => {
         const newColor = req.body.color;
         if (newCategory && newColor) {
             // Check the category has not already been added
-            index_category = categories.findIndex(t => t.name === newCategory);
+            index_category = categories.findIndex(t => t.category === newCategory);
             if (index_category > -1) {
                 console.log("Category already exists.")
             } else {
                 // Push new todo into array
-                categories.push({ name: newCategory , color: newColor });
-
-                // Write category to PostgreSQL and return the new ID
-                const result = await pool.query(
-                    'INSERT INTO categories (name, color) VALUES ($1, $2) RETURNING id',
-                    [newCategory, newColor]
-                );
-
-                // Get ID
-                const newCategoryId = result.rows[0].id;
-
-                // Add the new category to the object
-                categoryMap[newCategoryId] = newCategory;
+                categories.push({ category: newCategory , color: newColor });
+                // Write category to redis
+                await client.set('categories', JSON.stringify(categories));
+                console.log('Added new category:', newCategory);
             }
         }
         res.redirect("/categories");
@@ -262,27 +262,26 @@ app.post("/removecategory", async (req,res) => {
         const categorytoRemove = req.body.categorytoRemove;
         if (categorytoRemove) {
             // Split the task name and category
-            const [nameCategory, color] = categorytoRemove.split('_')
-            const categoryIndex  = categories.findIndex(t => t.name === nameCategory && t.color == color);
+            const [category, color] = categorytoRemove.split('_')
+            const categoryIndex  = categories.findIndex(t => t.category === category && t.color == color);
             // Remove from category database
             if (categoryIndex > -1) {
                 categories.splice(categoryIndex, 1);
-                console.log('Removed category:', nameCategory);
+                console.log('Removed category:', category);
             }
             // Remove tasks from todos with the removed category
-            var todosIndex = todos.findIndex(t => t.category == nameCategory);
+            var todosIndex = todos.findIndex(t => t.category == category);
             while (todosIndex > -1) {
                 todos.splice(todosIndex,1);
-                var todosIndex = todos.findIndex(t => t.category == nameCategory);
+                var todosIndex = todos.findIndex(t => t.category == category);
             }
             // Remove tasks from ticks with the removed category
-            var tickedIndex = ticks.findIndex(t => t.category == nameCategory);
+            var tickedIndex = ticks.findIndex(t => t.category == category);
             while (tickedIndex > -1) {
                 ticks.splice(tickedIndex,1);
-                var tickedIndex = ticks.findIndex(t => t.category == nameCategory);
+                var tickedIndex = ticks.findIndex(t => t.category == category);
             }
-            // Remove category from PostgreSQL
-            await pool.query('DELETE FROM categories WHERE name = $1', [nameCategory]);
+            await client.set('categories', JSON.stringify(categories));
         }
         res.redirect("/categories");
     } catch (err) {
@@ -295,34 +294,27 @@ app.post("/removecategory", async (req,res) => {
 app.post("/resetcategories", async (req,res) => {
     try{
         // Adds default categories
-        categories = [
-            { name: 'Work', color: '#0f74cc' },
-            { name: 'Home', color: '#5db7c6' },
-            { name: 'Garden', color: '#6b65da' }
-        ];
-        // Write categories to PostgreSQL
-        await pool.query('DELETE FROM categories');
-        for (let category of categories) {
-            await pool.query('INSERT INTO categories (name, color) VALUES ($1, $2)', [category.name, category.color]);
-            const tableName = `${category.name.toLowerCase()}_tasks`;
-            await pool.query(`CREATE TABLE IF NOT EXISTS ${tableName} (id SERIAL PRIMARY KEY, name TEXT NOT NULL, completed BOOLEAN DEFAULT FALSE)`);
-        }
-        console.log('Categories reseted!');
+        categories = [];
+        categories.push({category: 'Work', color: '#0f74cc'});
+        categories.push({category: 'Home', color: '#5db7c6'});
+        categories.push({category: 'Garden', color: '#6b65da'});
+        // Write in database
+        await client.set('categories', JSON.stringify(categories));
+        console.log('Categories reseted.');
         res.redirect("/categories");
     } catch (err) {
         console.error('Error reseting categories:', err);
         res.status(500).send('Internal Server Error');
     }
 })
-
-// Close the PostgreSQL client gracefully on server shutdown
+// Close the Redis client gracefully on server shutdown
 async function shutdown() {
     try {
-        await pool.end();
-        console.log("PostgreSQL client disconnected");
+        await client.quit();
+        console.log("Redis client disconnected");
         process.exit(0);
     } catch (err) {
-        console.error("Error during PostgreSQL client disconnection:", err);
+        console.error("Error during Redis client disconnection:", err);
         process.exit(1);
     }
 }
